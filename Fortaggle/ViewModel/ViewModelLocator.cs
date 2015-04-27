6b807f188629a24cc687faffac7a32de4ec6b236
gembaf <s15g464@stu.kagawa-u.ac.jp>
2015-04-27 18:31:34 +0900

    不要なファイルを削除

--- Fortaggle/ViewModel/ViewModelLocator.cs
+++ Fortaggle/ViewModel/ViewModelLocator.cs
@@ -12,7 +12,6 @@
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
 using Microsoft.Practices.ServiceLocation;
-using Fortaggle.Model;
 
 namespace Fortaggle.ViewModel
 {
@@ -28,16 +27,6 @@ namespace Fortaggle.ViewModel
         static ViewModelLocator()
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
-
-            if (ViewModelBase.IsInDesignModeStatic)
-            {
-                SimpleIoc.Default.Register<IDataService, Design.DesignDataService>();
-            }
-            else
-            {
-                SimpleIoc.Default.Register<IDataService, DataService>();
-            }
-
             SimpleIoc.Default.Register<MainViewModel>();
         }
 
