575369322730d21745b5b329aacf71eda04cab01
gembaf <s11t230@stmail.eng.kagawa-u.ac.jp>
2015-05-01 00:37:44 +0900

    Nameが空だった場合は警告を出すバリデーションを実装する

---
+++ Fortaggle/View/ValidationRules/TextBoxValidationRule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Fortaggle.View.ValidationRules
+{
+    public class TextBoxValidationRule : ValidationRule
+    {
+        //--- プロパティ
+
+        public bool CanEmpty { get; set; }
+
+        //--- コンストラクタ
+
+        public TextBoxValidationRule()
+        {
+            CanEmpty = false;
+        }
+
+        //--- ValidationResult
+
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            var v = value as String;
+
+            if (CanEmpty == false && String.IsNullOrEmpty(v))
+            {
+                return new ValidationResult(false, "入力して下さい");
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
