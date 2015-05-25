6cacc0b8135415a71f17a89a47f1c5f1e2067f50
gembaf <s11t230@stmail.eng.kagawa-u.ac.jp>
2015-05-25 17:19:26 +0900

    [add] refs #14 MainItemViewModelをItem系ページの中心とする

---
+++ Fortaggle/ViewModels/MainItemViewModel.cs
@@ -0,0 +1,42 @@
+﻿namespace Fortaggle.ViewModels
+{
+    using GalaSoft.MvvmLight;
+
+    public class MainItemViewModel : ViewModelBase
+    {
+        //--- 定数
+
+        //--- フィールド
+
+        //--- 静的コンストラクタ
+
+        //--- コンストラクタ
+
+        public MainItemViewModel()
+        {
+        }
+
+        //--- プロパティ
+
+        #region string Label 静的プロパティ
+
+        public static string Label
+        {
+            get { return "アイテム一覧"; }
+        }
+
+        #endregion
+
+        //--- public メソッド
+
+        //--- internal protected メソッド
+
+        //--- internal メソッド
+
+        //--- protected メソッド
+
+        //--- private メソッド
+
+        //--- static メソッド
+    }
+}
