7740dd3201d11b5087d7a83d29900b210c5806dd
gembaf <s15g464@stu.kagawa-u.ac.jp>
2015-06-02 15:59:03 +0900

    [add] refs #32 CheckboxDialogの表示を確認する

--- Fortaggle/ViewModels/MainItemViewModel.cs
+++ Fortaggle/ViewModels/MainItemViewModel.cs
@@ -1,11 +1,17 @@
 ﻿namespace Fortaggle.ViewModels
 {
+    using Fortaggle.ViewModels.Common;
+    using Fortaggle.ViewModels.Item;
     using Fortaggle.ViewModels.ItemGroup;
     using GalaSoft.MvvmLight;
+    using GalaSoft.MvvmLight.Command;
+    using System.Windows.Input;
 
     public class MainItemViewModel : ViewModelBase
     {
         //--- 定数
+
+        private static readonly ItemStatusServiceViewModel ItemStatusServiceVM = new ItemStatusServiceViewModel();
 
         //--- フィールド
 
@@ -32,6 +38,56 @@
         #region ItemGroupServiceViewModel ItemGroupServiceVM
 
         public ItemGroupServiceViewModel ItemGroupServiceVM { get; private set; }
+
+        #endregion
+
+        //--- 変更通知プロパティ
+
+        #region CheckboxDialogViewModel CheckboxDialogVM
+
+        private CheckboxDialogViewModel _CheckboxDialogVM;
+
+        public CheckboxDialogViewModel CheckboxDialogVM
+        {
+            get { return _CheckboxDialogVM; }
+            set
+            {
+                if (_CheckboxDialogVM != value)
+                {
+                    _CheckboxDialogVM = value;
+                    RaisePropertyChanged("CheckboxDialogVM");
+                }
+            }
+        }
+
+        #endregion
+
+        //--- コマンド
+
+        #region SelectItemStatusDialogCommand
+
+        private ICommand _SelectItemStatusDialogCommand;
+
+        public ICommand SelectItemStatusDialogCommand
+        {
+            get
+            {
+                if (_SelectItemStatusDialogCommand == null)
+                {
+                    _SelectItemStatusDialogCommand = new RelayCommand(
+                        () =>
+                        {
+                            CheckboxDialogVM = new CheckboxDialogViewModel(
+                                () =>
+                                {
+                                    CheckboxDialogVM = null;
+                                },
+                                ItemStatusServiceVM.ItemStatusVMList);
+                        });
+                }
+                return _SelectItemStatusDialogCommand;
+            }
+        }
 
         #endregion
 
