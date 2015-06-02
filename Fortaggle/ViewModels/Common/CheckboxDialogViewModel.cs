111f01de330b8fda91a26522c44eb1e739deeec8
gembaf <s15g464@stu.kagawa-u.ac.jp>
2015-06-02 15:57:30 +0900

    [add] refs #32 Checkbox表示用のViewModelとViewを実装する

---
+++ Fortaggle/ViewModels/Common/CheckboxDialogViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace Fortaggle.ViewModels.Common
+{
+    using GalaSoft.MvvmLight;
+    using System;
+	using System.Collections.Generic;
+
+    public class CheckboxDialogViewModel : ViewModelBase
+    {
+        //--- 定数
+
+        //--- フィールド
+
+        private Action closeAction;
+
+        //--- 静的コンストラクタ
+
+        //--- コンストラクタ
+
+        public CheckboxDialogViewModel(Action closeAction, IEnumerable<ViewModelBase> list)
+        {
+            this.closeAction = closeAction;
+            VMList = new List<ViewModelBase>(list);
+        }
+
+        //--- プロパティ
+
+        #region List<ViewModelBase>
+
+        public List<ViewModelBase> VMList { get; set; }
+
+        #endregion
+
+        //--- public メソッド
+
+        //--- protected メソッド
+
+        //--- private メソッド
+
+        //--- static メソッド
+    }
+}
